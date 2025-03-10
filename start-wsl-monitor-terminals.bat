b7cb1c078f17293616a8efac95f9fb5b4163c417
Alice Lépissier <alice.lepissier@gmail.com>
2025-03-09 21:38:44 -0400

    Update start-wsl-monitor-terminals.bat

--- start-wsl-monitor-terminals.bat
+++ start-wsl-monitor-terminals.bat
@@ -2,3 +2,4 @@
 start wsl -e bash -c "cd ~ && systemctl status nginx; exec bash"
 start wsl -e bash -c "cd ~ && systemctl status rstudio-server; exec bash"
 start wsl -e bash -c "cd ~ && systemctl status jupyterhub; exec bash"
+/pause
