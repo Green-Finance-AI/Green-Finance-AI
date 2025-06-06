a1f0daccc234f3752da7d0ba3213f34a8c687a04
Alice Lépissier <alice.lepissier@gmail.com>
2025-06-06 14:39:31 -0400

    Move monitoring bat file from GFAI to Training-Platform repo

--- start-wsl-monitor-terminals.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start wsl -e bash -c "cd ~ && systemctl status nginx; exec bash"
-start wsl -e bash -c "cd ~ && systemctl status rstudio-server; exec bash"
-start wsl -e bash -c "cd ~ && systemctl status jupyterhub; exec bash"
-/pause
